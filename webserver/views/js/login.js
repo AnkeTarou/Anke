b2d74d78aa033b30e4d9c4d86f1e7d9c58b95eb6
bisce <iyi2adpke@i.softbank.jp>
2018-06-05 16:14:13 +0900

    更新

---
+++ webserver/views/js/login.js
@@ -0,0 +1,37 @@
+/*
+  ログインの定義
+ */
+ var login = {
+   init:function(){
+     this.userIdNode  = document.getElementById("inputloginid");
+     this.userPassNode = document.getElementById("inputloginpass");
+     this.inSub = document.getElementById("inputlogin");
+     this.outSub = document.getElementById("inputlogout");
+     this.inSub.onclick = this.route;
+   }
+ };
+ //ログイン処理
+ login.route = function(){
+   $.ajax({
+     type: "POST",
+     url: "/login/",
+     dataType: 'json',
+     data:{_id:login.userIdNode.value,pass:login.userPassNode.value}
+   })
+   .done(function(res){
+     if(res.boo == 1){
+         console.log("ログイン成功");
+         // cookieに値をセット
+         document.cookie = 'userid=' + res.userid;
+         document.cookie = 'userpass=' + res.userpass;
+         console.log(document.cookie);
+
+         user = {_id:res.userid,_pass:res.userpass};
+     }else{
+         console.log("ログイン失敗");
+     }
+   })
+   .fail(function(res){
+     console.error(res);
+   });
+ };
