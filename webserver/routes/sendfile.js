6fd905c993ac393171eace76ad78e1e1cef6f8c0
tutitona <39608217+tutitona@users.noreply.github.com>
2018-06-08 12:30:32 +0900

    画像アップ

--- webserver/routes/sendfile.js
+++ webserver/routes/sendfile.js
@@ -2,13 +2,7 @@ const dbo = require('../lib/mongo');
 
 // 検索処理　１単語のみの検索に対応
 exports.post = function(req,res){
-  const key = {$regex:".*"+req.body.value+".*"};
-  const keyObj = [
-    {$match:{$or:[{query:key},{"ansers.anser":key}]}},
-    {$project:{query:1,[`ansers.anser`]:1,total:{$sum:"$ansers.total"}}},
-    {$sort:{total:-1}}
-  ];
-  dbo.aggregate("QuestionData","question",keyObj,function(JSON){
-    res.json(JSON);
-  });
+    console.log(req.body.data);
+    //dbo.insert("UserData","user",data);
+    res.json(true);
 };
