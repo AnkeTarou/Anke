343545ce7642830e06f45676dbb05782c84ef8d0
tutitona <39608217+tutitona@users.noreply.github.com>
2018-06-08 12:32:36 +0900

    あああ

--- webserver/routes/sendfile.js
+++ webserver/routes/sendfile.js
@@ -5,4 +5,13 @@ exports.post = function(req,res){
     console.log(req.body.data);
     //dbo.insert("UserData","user",data);
     res.json(true);
+  const key = {$regex:".*"+req.body.value+".*"};
+  const keyObj = [
+    {$match:{$or:[{query:key},{"answers.answer":key}]}},
+    {$project:{query:1,[`answers.answer`]:1,total:{$sum:"$answers.total"}}},
+    {$sort:{total:-1}}
+  ];
+  dbo.aggregate("QuestionData","question",keyObj,function(JSON){
+    res.json(JSON);
+  });
 };
