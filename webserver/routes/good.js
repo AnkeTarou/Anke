74bc273f1b463d71ce2fc9109ead14d03e6ed8cc
utiyama5748 <39608214+utiyama5748@users.noreply.github.com>
2018-06-14 11:07:28 +0900

    更新

--- webserver/routes/good.js
+++ webserver/routes/good.js
@@ -2,9 +2,19 @@ const dbo = require('../lib/mongo');
 
 exports.post = function(req,res){
   const param = req.body;
-  const key = [{$match:{_id:require('mongodb').ObjectID(param.id)}}];
-  dbo.good(param.user,param.id,param.good,key,function(JSON){
-    console.log(JSON[0]);
-    res.json(JSON[0]);
+  const key = [
+    {$match:{_id:require('mongodb').ObjectID(param.id)}},
+    {$project:{totalGood:{$size:"$good"}}}
+  ];
+
+  dbo.userCheck(param.user,function(result){
+    if(result){
+      dbo.good(param.user,param.id,param.good,key,function(JSON){
+        console.log(JSON[0]);
+        res.json(JSON[0]);
+      });
+    }else{
+      res.json(null);
+    }
   });
 }
