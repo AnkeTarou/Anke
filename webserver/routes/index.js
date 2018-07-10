64323d35dc7d8a6d25fbcb54eec31e35af926531
utiyama5748 <39608214+utiyama5748@users.noreply.github.com>
2018-07-10 11:54:24 +0900

    更新

--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -7,33 +7,21 @@ const search = require('./search');
 const vote = require('./vote');
 const favorite = require('./favorite');
 const home = require('./home');
+const login = require('./login');
 const follow = require('./follow');
 //const follow = require('./follow');
 
-router.use(function(req,res,next){
-  if(req.session.user){
-    next();
-  }else{
-    if(req.url == '/login' || req.url == '/search' || req.url == '/post'){
-      next();
-    }else {
-      res.redirect('/login');
-    }
-  }
-});
-
 router.get('/',home.get);
 router.post('/home',home.post);
+
+router.get('/search',search.get);
+router.post('/search',search.post);
 
 router.post('/post',post.post);
 
 router.post('/vote',vote.post);
 
 router.post('/favorite',favorite.post);
-
-router.get('/search',search.get);
-router.post('/search',search.post);
-
 
 router.get('/follow',follow.get);
 
@@ -44,5 +32,6 @@ router.get('/login',function(req,res){
     res.render('login');
   }
 });
+router.post('/login',login.post);
 
 module.exports = router;
