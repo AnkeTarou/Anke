27ef0df14f06d40adb48ebaf1e5019aa6a44cbb5
utiyama5748 <39608214+utiyama5748@users.noreply.github.com>
2018-07-10 09:39:43 +0900

    session

--- webserver/app.js
+++ webserver/app.js
@@ -24,7 +24,7 @@ app.use(session({
 // アクセスしてきたときに最初に実行される処理
 app.use(function(req,res,next){
   console.log("url\n"+req.url);
-  //req.session.user = {_id:"testuser",sessionkey:"sessionキー"};
+  req.session.user = {_id:"testuser",sessionkey:"sessionキー"};
   console.log("セッション\n"+JSON.stringify(req.session));
   console.log("受信データ\n");
   console.log("POST\n",JSON.stringify(req.body));
